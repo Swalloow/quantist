d66dd26ed74b3e4e9203f4b81597bc2a2ca155c7
Swalloow <joon920570@gmail.com>
2017-08-07 23:24:14 +0900

    Add database module with api test

---
+++ Rogers-Crawler/main.py
@@ -0,0 +1,25 @@
+from rogers.blockchain.bithumb import ParserBithumb
+from rogers.blockchain.coinone import ParserCoinone
+from rogers.blockchain.korbit import ParserKorbit
+from rogers.model.database import Database
+
+
+def main():
+    parser_class = {
+        "bithumb": ParserBithumb,
+        "coinone": ParserCoinone,
+        "korbit": ParserKorbit
+    }
+
+    db = Database()
+    for Parser in parser_class.values():
+        parser = Parser()
+
+        for currency in parser.currency:
+            params = {"currency": currency}
+            response = parser.get_response(params)
+            model = parser.parse(response, currency)
+            db.insert(model)
+
+if __name__ == '__main__':
+    main()
