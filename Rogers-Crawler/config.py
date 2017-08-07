b5c23df3a2054a16de44c0f414b48a7fc61829cb
Swalloow <joon920570@gmail.com>
2017-08-07 23:22:59 +0900

    Initial project structure

---
+++ Rogers-Crawler/config.py
@@ -0,0 +1,9 @@
+class Config:
+    APP_NAME = 'rogers'
+    DATABASE_URI = 'database_uri'
+
+
+class Source:
+    BITHUMB_URI = 'https://api.bithumb.com/public/ticker/'
+    COINONE_URI = 'https://api.coinone.co.kr/ticker/'
+    KORBIT_URI = 'https://api.korbit.co.kr/v1/ticker/detailed'
